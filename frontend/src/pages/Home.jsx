815f8e5759d7553e90242b90f5441b224b3bd863
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-24 12:22:33 +0530

    Header commit
    Header Section done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Header from '../components/Header'
+
+
+
 
 const Home = () => {
+
+
   return (
+
+
     <div>
-      Home
+      <Header />
     </div>
+
+
   )
+
 }
 
 export default Home
