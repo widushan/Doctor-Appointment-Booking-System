ed5b5a148b9f75b33454d775e781cceb21b1b2a7
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-29 00:06:07 +0530

    related commit
    Build Related Doctors section

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,26 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 
-
-
-
-const TopDoctors = () => {
-
-  const navigate = useNavigate();
+const RelatedDoctors = ({ speciality, docId }) => {
 
   const { doctors } = useContext(AppContext)
+  const navigate = useNavigate()
 
+  const [relDoc, setRelDocs] = useState([])
+
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter((doc) => doc.speciality === speciality && doc._id !== docId)
+
+      setRelDocs(doctorsData)
+    }
+  }, [doctors, speciality, docId])
 
   return (
-
-
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-      <h1 className='text-3xl font-medium'>Top Doctors Available for Consultation</h1>
-      <p className='sm:w-1/3 text-center text-sm'>Browse through our list of experienced and trusted doctors. Choose the right specialist and book your appointment in just a few clicks.</p>
+      <h1 className='text-3xl font-medium'>Related Doctors</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Other doctors with the same speciality.</p>
 
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {doctors.slice(0, 10).map((item, index) => (
+        {relDoc.slice(0, 5).map((item, index) => (
           <div
             onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }}
             className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
@@ -41,10 +44,7 @@ const TopDoctors = () => {
 
 
     </div>
-
-
   )
-
 }
 
-export default TopDoctors
+export default RelatedDoctors
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 
 const Appointment = () => {
@@ -134,6 +135,10 @@ const Appointment = () => {
         <button className='bg-primary text-white text-sm font-light px-14 py-2 rounded-full my-6'>Book Appointment</button>
 
       </div>
+
+
+      {/* Listing Related Doctors */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
 
 
     </div>
