0b5dab676a86dee5fa77e12f0a81e1ae46bdfaee
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-29 00:32:51 +0530

    about commit
    Build About Us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+
+
 
 const About = () => {
+
+
   return (
+
     <div>
-      About
+
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to MedixGo, your trusted partner in managing your healthcare needs conveniently and efficiently. At MedixGo, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
+          <p>MedixGo is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, MedixGo is here to support you every step of the way.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at MedixGo is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+        </div>
+      </div>
+
+
+      <div className='text-xl my-4'>
+        <p>Why <span className='text-gray-700 font-semibold'>Choose Us</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20 gap-10'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency - </b>
+          <p>Streamline your healthcare journey with our intuitive platform, designed to simplify appointment scheduling and access to medical care.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience - </b>
+          <p>Access quality healthcare from the comfort of your home with our user-friendly interface and 24/7 availability.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization - </b>
+          <p>Receive tailored healthcare recommendations and appointment suggestions based on your specific needs and preferences.</p>
+        </div>
+      </div>
+
     </div>
+
   )
+
 }
 
 export default About
