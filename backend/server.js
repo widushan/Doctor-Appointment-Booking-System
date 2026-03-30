90554c66ed0b38e2563890ef0761b71aede46f83
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-31 01:19:49 +0530

    db commit
    started to connect DB process

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import mongoose from "mongoose";
+import connectDB from "./config/mongodb.js";
+
 
 
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 
 // middleware
