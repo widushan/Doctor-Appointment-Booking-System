f6842c19f186207b5d4184731eeedf9e5393cdc5
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-30 17:18:46 +0530

    mobile commit
    Mobile Nav design created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 
@@ -7,57 +7,89 @@ import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    const [token, setToken] = useState(true);
+  const [token, setToken] = useState(true);
 
   return (
 
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
 
       <img
-      onClick={()=>navigate('/')}
-       className='w-44 cursor-pointer' src={assets.MedixGo} alt="" />
+        onClick={() => navigate('/')}
+        className='w-44 cursor-pointer' src={assets.MedixGo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-semibold'>
         <NavLink to='/'>
-            <li className='py-1'>HOME</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>HOME</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/doctors'>
-            <li className='py-1'>ALL DOCTORS</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>ALL DOCTORS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/about'>
-            <li className='py-1'>ABOUT</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>ABOUT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
         <NavLink to='/contact'>
-            <li className='py-1'>CONTACT</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+          <li className='py-1'>CONTACT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
         {
-            token ?
+          token ?
             <div className='flex items-center gap-2 cursor-pointer group relative'>
-                <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-                <img className='w-2.5 ' src={assets.dropdown_icon} alt="" />
-                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                        <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                        <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                        <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
-                    </div>
+              <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+              <img className='w-2.5 ' src={assets.dropdown_icon} alt="" />
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                  <p onClick={() => navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                  <p onClick={() => navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                 </div>
+              </div>
             </div>
             :
-            <button 
-        onClick={()=>navigate('/login')}
-        className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            <button
+              onClick={() => navigate('/login')}
+              className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
         }
-        
+
+        <img
+          onClick={() => setShowMenu(true)}
+          className='w-6 md:hidden cursor-pointer' src={assets.menu_icon} alt="" />
+        {/*----------------------Mobile Menu----------------------*/}
+        {showMenu && (
+          <div className='fixed inset-0 bg-black bg-opacity-50 z-50'>
+            <div className='absolute right-0 top-0 w-64 bg-white h-full'>
+              <img
+                onClick={() => setShowMenu(false)}
+                className='w-6 absolute top-4 right-4 cursor-pointer' src={assets.cross_icon} alt="" />
+              <ul className='flex flex-col gap-4 p-4 items-center'>
+                <NavLink to='/' onClick={() => setShowMenu(false)}>
+                  <p className='px-4 py-2 rounded inline-block'>HOME</p>
+
+                </NavLink>
+                <NavLink to='/doctors' onClick={() => setShowMenu(false)}>
+                  <p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p>
+
+                </NavLink>
+                <NavLink to='/about' onClick={() => setShowMenu(false)}>
+                  <p className='px-4 py-2 rounded inline-block'>ABOUT</p>
+
+                </NavLink>
+                <NavLink to='/contact' onClick={() => setShowMenu(false)}>
+                  <p className='px-4 py-2 rounded inline-block'>CONTACT</p>
+
+                </NavLink>
+              </ul>
+            </div>
+          </div>
+        )}
+
       </div>
 
     </div>
