2989e682c9a9baf510ad7b8efd8d471978e4199d
Pasindu Widushan <114930601+widushan@users.noreply.github.com>
2026-03-31 22:35:25 +0530

    addDoc commit
    Build Add Doctor API

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import DoctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
@@ -64,4 +65,26 @@ const addDoctor = async (req, res) => {
     }
 }
 
-export { addDoctor }
+
+// API for Admin logging
+const loginAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({ success: true, token })
+
+        } else {
+            res.json({ success: false, message: "Invalid credentials" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+
+export { addDoctor, loginAdmin }
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { addDoctor } from '../controllers/adminController.js'
+import { addDoctor, loginAdmin } from '../controllers/adminController.js'
 import upload from '../middleware/multer.js'
+import authAdmin from '../middleware/authAdmin.js'
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor)
+adminRouter.post('/login', loginAdmin)
 
 export default adminRouter
